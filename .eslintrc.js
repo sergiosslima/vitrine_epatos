dad9826dbb7082c881691a0aaacfb70ba2a4f077
Sérgio Lima <sergio@agenciapropagando.com.br>
2022-07-10 23:46:58 -0300

    Versão com problemas

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,15 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
+  'env': {
+    'browser': true,
+    'es2021': true,
   },
   'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended'
+    'google',
   ],
-  parserOptions: {
-    parser: '@babel/eslint-parser'
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
   },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+  'rules': {
+  },
+};
